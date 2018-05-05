2ee188b2c99751fbd4c477a1edcca056314950f1
Justin Richardsson <2107110+hallaathrad@users.noreply.github.com>
2018-05-05 14:31:36 -0400

    add first actual member
    This is a small step for a machine, yet a big step for @freeCodeCampTO

--- index.js
+++ index.js
@@ -2,13 +2,19 @@ const appContainer = document.getElementById("app");
 
 // be nice or else!!! :D
 const users = [
-  //example
+  // example.
+  /*
   {
     name: "someone",
     bio: "I am an awesome person",
-    githubUser: "hlminh2000",
-    githubProfile: "https://github.com/hlminh2000"
-  }
+    github: "@johndoe",
+  },
+  */
+  {
+    name: "Justin Sane",
+    bio: "⚛️ Senior JS Developer @TelusDigital || @FreeCodeCamp core || Working on my own garden. Cannot care if your side of the hedge seems greener. || INTJ-A Úlfhéðin",
+    github: "@hallaathrad"
+  },
 ];
 
 const toHtml = users =>
@@ -18,9 +24,9 @@ const toHtml = users =>
         <div class="is-size-2"> ${user.name} </div>
         <div class="is-size-4">
           <img class="octocat" src="${"https://assets-cdn.github.com/images/modules/logos_page/GitHub-Mark.png"}"/>
-          <a href="${user.githubProfile}">${user.githubUser}</a>
+          <a href="http://github.com/${user.github}">${user.github}</a>
         </div>
-        <div class="is-size-6"> ${user.bio} </div>
+        <div class="is-size-6">${user.bio}</div>
       </div>
       `
   );
